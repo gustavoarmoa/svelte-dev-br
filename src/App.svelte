b3b35d53e3ffdde51e4be1941dfdd643a3ee3018
gustavoarmoa <gustavo.armoa@outlook.com>
2021-10-18 08:58:15 -0300

    Tradução inicial e deploy do projeto

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,10 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Olá, mundo do <br> <strong>{name}</strong>!</h1>
+	<p>Este é um projeto open source para traduzir ao português a documentação oficial do <strong><a href="https://svelte.dev">Svelte</a></strong> e trazer case de usos no mercado nacional.</p>
+	<p>Se quiser contribuir pode encaminhar sua <strong>PR</strong> Link do <a href="https://github.com/gustavoarmoa/svelte-dev-br">repositório no Github</a></p>
+	<p>Visite o <a href="https://svelte.dev/tutorial">Tutorial do Svelte</a> e aprenda como construir uma webapp em Svelte.</p>
 </main>
 
 <style>
